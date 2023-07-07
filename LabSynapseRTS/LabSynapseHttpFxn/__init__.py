5710ef84c397284f1f816eb30c994dab925d65a8
DianaMDB <104025201+DianaMDB@users.noreply.github.com>
2023-07-07 23:04:34 +0530

    Update __init__.py added operationType

--- LabSynapseRTS/LabSynapseHttpFxn/__init__.py
+++ LabSynapseRTS/LabSynapseHttpFxn/__init__.py
@@ -2,7 +2,6 @@ import json
 import logging
 import os
 import azure.functions as func
-from azure.identity import DefaultAzureCredential
 from azure.storage.filedatalake import DataLakeServiceClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -15,21 +14,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
    storage_file_name = os.environ["storage_file_name"]
    service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
            "https", storage_account_name), credential=storage_account_key)
-   #object_id = req.get_body().__getitem__(0)
    json_data = req.get_body()
    logging.info(json_data)
    object_id = "test"
    try:
        json_string = json_data.decode("utf-8")
        json_object = json.loads(json_string)
-       object_id = json_object["_id"]["$oid"]
+
+       if json_object["operationType"] == "delete":
+           object_id = json_object["fullDocumentBeforeChange"]["_id"]["$oid"]
+           data = {"operationType": json_object["operationType"], "data":json_object["fullDocumentBeforeChange"]}
+       else:
+           object_id = json_object["fullDocument"]["_id"]["$oid"]
+           data = {"operationType": json_object["operationType"], "data":json_object["fullDocument"]}
+       
        logging.info(object_id)
+       encoded_data = json.dumps(data)
    except Exception as e:
        logging.info("Exception occured : "+ str(e)) 
-         
+       
    file_system_client = service_client.get_file_system_client(file_system=storage_container)
    directory_client = file_system_client.get_directory_client(storage_directory)
    file_client = directory_client.create_file(storage_file_name + "-" + str(object_id) + ".txt")
-   file_client.append_data(data=json_data, offset=0, length=len(json_data))
-   file_client.flush_data(len(json_data))
+   file_client.append_data(data=encoded_data, offset=0, length=len(encoded_data))
+   file_client.flush_data(len(encoded_data))
    return func.HttpResponse(f"This HTTP triggered function executed successfully.")
